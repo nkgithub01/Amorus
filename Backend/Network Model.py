de3bc6d2b82e1b760715da3d17e2e9d7ac9ff276
Elijah-Huang <elijahjhuang@gmail.com>
2021-02-13 03:37:31 -0800

    updated population + generator

--- Backend/Network Model.py
+++ Backend/Network Model.py
@@ -1,8 +1,10 @@
-def imp():
-    import pandas as pd
-    import tensorflow as tf
+import pandas as pd
+import tensorflow as tf
 
-population = pd.lo
+#read in saved population data
+#first 2 elements are name and neighbors, rest are the trainable features
+population = pd.read_csv("population.csv")
+
 # putting strings into a linear regression seems like a very bad idea
 # (you would be doing theta*string instead of theta*numerical_value for that part of the line)
 # so let's convert all categorical data (here stuff with strings) into something numerical
@@ -19,13 +21,41 @@ for feature in d_train:
 
 features = ['age', 'status', 'sex', 'orientation', 'body_type', 'diet', 'drinks', 'drugs', 'education',
                        'ethnicity', 'height', 'income', 'job', 'offspring', 'pets', 'religion', 'smokes', 'speaks']
+
+# vocabulary list for categorical data
+# the order matters for some features (categories listed in ascending order of drinking level for example)
+Vocabulary = { 'sex' : ['male', 'female'],
+     'status' : ['single', 'seeing someone'],
+    'sexual orientation' : ['straight', 'gay'],
+     'body_type' : ['skinny', 'thin', 'average', 'fit', 'athletic', 'curvy', 'a little extra', 'full figured', 'jacked'],
+    'diet' : ['strictly other', 'mostly other', 'other', 'strictly vegetarian', 'mostly vegetarian', 'vegetarian',
+              'strictly anything', 'mostly anything', 'anything'],
+     'drinks' : ['not at all', 'rarely', 'socially', 'often', 'desperately'],
+    'drugs' : ['never', 'sometimes'],
+     'education' : ['high school', 'dropped out', 'working', 'graduated'],
+    'ethnicity' : ['white', 'native', 'middle', 'hispanic', 'black', 'asian', 'pacific', 'indian'],
+     'job title' : ['transportation', 'hospitality', 'student', 'artistic', 'computer', 'science', 'banking', 'sales',
+                    'medicine', 'executive', 'clerical', 'construction', 'political', 'law', 'education', 'military'],
+    'offspring' : ["doesn't want", "might want", "doesn't have kids", "has", "wants"],
+     'pets' : ["likes dogs and cats", "likes dogs", "likes cats", "has dog", "has cat"],
+    'religion' : ['agnostic', 'atheism', 'buddhism', 'catholicism', 'very serious', 'somewhat serious',
+                  'not too serious', 'laughing'],
+     'smokes' : ["no","trying to quit", "sometimes", "when drinking", "yes"],
+    'speaks' : ["english", "spanish", "french", "german", "sign language", "italian", "japanese", "russian", "gujarati",
+                "hindi", "chinese", "sanskrit", "portuguese"]}
+
 class User:
     def __init__(self, make_linear_classifier = True):
         self.features = dict(zip(features, range(len(features))))
+        self.neighbors = []
         self.linear_classifier = None
 
+        # setup person's features and
         # setup linear_classifier
+
+        # user needs to input info to create this linear classifier
         if make_linear_classifier:
+
 
 
 def main():
--- Backend/Populating Site.py
+++ Backend/Populating Site.py
@@ -1,7 +1,7 @@
 import pandas as pd
 all = pd.read_csv("okcupid_profiles.csv") # the entire dataset
 
-features_to_include = {'name', 'age', 'status', 'sex', 'orientation', 'body_type', 'diet', 'drinks', 'drugs', 'education',
+features_to_include = {'name', 'neighbors', 'age', 'status', 'sex', 'orientation', 'body_type', 'diet', 'drinks', 'drugs', 'education',
                        'ethnicity', 'height', 'income', 'job', 'offspring', 'pets', 'religion', 'smokes', 'speaks'}
 
 features_to_not_include = []
@@ -9,6 +9,7 @@ for feature in all:
     if feature not in features_to_include:
         features_to_not_include.append(feature)
 
+# delete unwanted features
 for feature in features_to_not_include:
     del all[feature]
 
@@ -17,7 +18,9 @@ names_file = pd.read_csv("60knames.csv")
 names = [names_file.loc[i][0] + ' ' + names_file.loc[i][1] for i in range(names_file.shape[0])]
 
 #insert the names as the first column in the dataframe
-all.insert(0,'name',names[:all.shape[0]])
+all.insert(0,'neighbors', [[] for i in range(all.shape[0])])
+all.insert(0,'name', names[:all.shape[0]])
+
 
 #check that features we want are left
 print(set(all.keys()) == features_to_include, '\n')
@@ -25,6 +28,7 @@ print(all.head())
 print(all.shape)
 
 #create the population output file
+#first 2 elements are name and neighbors, rest are the trainable features
 all.to_csv("population.csv")
 
 
