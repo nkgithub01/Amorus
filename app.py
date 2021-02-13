d2768ec6a0a5741e581febc6e18696c97d18ba93
Arhan Jain <arhan.j04@gmail.com>
2021-02-13 05:55:33 -0800

    Login Functionality
    project structure is creating some URL problems

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ def homePage():
 def matchmake():
     return render_template("matchmake.html", messageForEli=request.form['mensaje'])
 
+@app.route("/login", methods=['GET','POST'])
+def loginPage():
+    return render_template("login.html")
+
+@app.route("/doLogin", methods=['GET', 'POST'])
+def doLogin():
+    #do bunch of sign in shit   
+    return render_template("homeLoggedIn", handle=request.form['handle'])
