a87c7201e21f46e7022604d19173dc4a69651ee9
Arhan Jain <arhan.j04@gmail.com>
2021-02-12 17:02:18 -0800

    Initialized HTML Home Page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def homePage():
-    return "Amorus"
+    return render_template("index.html")
