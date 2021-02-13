95ce71614f0d416f05c700dc518db82b89ced914
Elijah-Huang <elijahjhuang@gmail.com>
2021-02-13 04:43:14 -0800

    Updated Network Model

--- Backend/Network Model.py
+++ Backend/Network Model.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 #first 2 elements are name and neighbors, rest are the trainable features
 population = pd.read_csv("population.csv")
 
+'''
 # putting strings into a linear regression seems like a very bad idea
 # (you would be doing theta*string instead of theta*numerical_value for that part of the line)
 # so let's convert all categorical data (here stuff with strings) into something numerical
@@ -18,13 +19,14 @@ for feature in d_train:
     else:
         # the datatype is a float
         feature_columns.append(tf.feature_column.numeric_column(feature, dtype=tf.float32))
-
-features = ['age', 'status', 'sex', 'orientation', 'body_type', 'diet', 'drinks', 'drugs', 'education',
+'''
+# features that a person will have
+features = ['name','neighbors', 'age', 'status', 'sex', 'orientation', 'body_type', 'diet', 'drinks', 'drugs', 'education',
                        'ethnicity', 'height', 'income', 'job', 'offspring', 'pets', 'religion', 'smokes', 'speaks']
 
-# vocabulary list for categorical data
+# category/ vocabulary list for features that have categorical data
 # the order matters for some features (categories listed in ascending order of drinking level for example)
-Vocabulary = { 'sex' : ['male', 'female'],
+categories = { 'sex' : ['male', 'female'],
      'status' : ['single', 'seeing someone'],
     'sexual orientation' : ['straight', 'gay'],
      'body_type' : ['skinny', 'thin', 'average', 'fit', 'athletic', 'curvy', 'a little extra', 'full figured', 'jacked'],
@@ -44,22 +46,60 @@ Vocabulary = { 'sex' : ['male', 'female'],
     'speaks' : ["english", "spanish", "french", "german", "sign language", "italian", "japanese", "russian", "gujarati",
                 "hindi", "chinese", "sanskrit", "portuguese"]}
 
+adj = [population.loc[i]['neighbors'] for i in range(population.shape[0])]
+
 class User:
-    def __init__(self, make_linear_classifier = True):
+    def __init__(self, user_features = None, make_linear_classifier = True):
+        # dict of features that have user_features as values
         self.features = dict(zip(features, range(len(features))))
-        self.neighbors = []
+        #user features list
+        self.features_list = []
+        #linear regression model
         self.linear_classifier = None
 
-        # setup person's features and
+        # setup person's features
+        if user_features is None:
+            # user needs to input their personal features
+
+            print("Enter your name")
+            inp = input().strip()
+            self.features_list.append(inp)
+            self.features['name'] = inp
+
+            for feature in features[2:]:
+                if feature in categories:
+                    #categorical feature
+                    print("enter the number of the option that best describes you")
+                    for a,b in enumerate(categories[feature]):
+                        print(str(a) + '. ' + b)
+                    inp = int(input())
+                    self.features_list.append(categories[feature][inp])
+                    self.features[feature] = categories[feature][inp]
+
+                else:
+                    #numerical feature
+                    print('Enter your ' + feature + ':')
+                    inp = int(input())
+                    self.features_list.append(inp)
+                    self.features[feature] = inp
+
+            print("Now let's connect you to your friends :)")
+            while(True):
+                print('Enter the name of the person who you are friends with or enter \"amorus\" to stop')
+                inp = input().strip()
+
+                if inp == 'amorus':
+                    break
+                else:
+
+
+
         # setup linear_classifier
-
-        # user needs to input info to create this linear classifier
         if make_linear_classifier:
-
-
+            # user needs to input who they like to create this linear classifier
+            pass
 
 def main():
-    pass
 
 
 main()
