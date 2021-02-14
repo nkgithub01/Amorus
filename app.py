5f68360b660e47082b682adb80aad28862e048c0
Arhan Jain <arhan.j04@gmail.com>
2021-02-13 21:49:42 -0800

    Front Side MatchMake done, Sign In Working

--- app.py
+++ app.py
@@ -3,14 +3,18 @@ import pandas as pd
 from os import path
 
 app = Flask(__name__)
+isSignedIn = False
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/matchmaker', methods=['GET', 'POST'])
+@app.route('/matchmaker')
 def matchmaker():
-    return render_template('matchmake.html', messageForEli=request.form['mensaje'])
+    if isSignedIn:
+        return render_template('matchmake.html')
+    else:
+        return render_template("login.html")
 
 @app.route("/signIn", methods=['GET','POST'])
 def signIn():
@@ -18,6 +22,9 @@ def signIn():
 
 @app.route("/doLogin", methods=['GET', 'POST'])
 def doLogin():
+    global isSignedIn
+    isSignedIn = True
+
     if path.exists("/loginData.csv"):
         return "hello"
     else:
@@ -26,3 +33,8 @@ def doLogin():
         df.to_csv('loginData.csv')
     #do bunch of sign in shit   
     return render_template("homeLoggedIn.html", handle=request.form['handle'])  
+
+@app.route("/beginSearch", methods=['GET', 'POST'])
+def beginSearch():
+    #start search algorithm
+    return render_template("searchResults.html")
