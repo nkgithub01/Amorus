e9211f4d0dba00730a4dd05e2dc9148e194b09fc
Arhan Jain <arhan.j04@gmail.com>
2021-02-13 19:19:56 -0800

    CSS Update

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask, render_template, request
+import pandas as pd
+from os import path
 
 app = Flask(__name__)
 
-@app.route("/")
-def homePage():
-    return render_template("index.html")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-@app.route("/matchmake", methods=['GET', 'POST'])
-def matchmake():
-    return render_template("matchmake.html", messageForEli=request.form['mensaje'])
+@app.route('/matchmaker', methods=['GET', 'POST'])
+def matchmaker():
+    return render_template('matchmake.html', messageForEli=request.form['mensaje'])
 
-@app.route("/login", methods=['GET','POST'])
-def loginPage():
+@app.route("/signIn", methods=['GET','POST'])
+def signIn():
     return render_template("login.html")
 
 @app.route("/doLogin", methods=['GET', 'POST'])
 def doLogin():
+    if path.exists("/loginData.csv"):
+        return "hello"
+    else:
+        d = {'AmorusHandle': [], 'Password': []}
+        df = pd.DataFrame(data=d)
+        df.to_csv('loginData.csv')
     #do bunch of sign in shit   
-    return render_template("homeLoggedIn", handle=request.form['handle'])
+    return render_template("homeLoggedIn.html", handle=request.form['handle'])  
