2bfb46369c99e4b17b51e810572b3c2ffa9e7ac9
Arhan Jain <arhan.j04@gmail.com>
2021-02-15 20:54:31 -0800

    Restructured Folder

--- FrontEnd/app.py
+++ FrontEnd/app.py
@@ -13,11 +13,11 @@ userFeatures = [None]*20
 profilesShown = 0
 training_labels = []
 
-dataSet = pd.read_csv("Backend/population.csv")
+dataSet = pd.read_csv("../Backend/population.csv")
 
 @app.route('/')
 def home():
-    return render_template('searchResults.html')
+    return render_template('index.html')
 
 @app.route('/matchmaker')
 def matchmaker():
@@ -64,32 +64,36 @@ def returnRandomProfile():
 def uploadData():
     global isSignedIn
     global userFeatures
+    try:
+        isSignedIn = True
 
-    isSignedIn = True
+        #oh god
+        userFeatures[0] = request.form['name']
+        userFeatures[1] = request.form['friends']
+        userFeatures[2] = request.form['age']
+        userFeatures[3] = request.form['status']
+        userFeatures[4] = request.form['sex']
+        userFeatures[5] = request.form['orientation']
+        userFeatures[6] = request.form['bodyType']
+        userFeatures[7] = request.form['diet']
+        userFeatures[8] = request.form['drinks']
+        userFeatures[9] = request.form['drugs']
+        userFeatures[10] = request.form['education']
+        userFeatures[11] = request.form['ethnicity']
+        userFeatures[12] = request.form['height']
+        userFeatures[13] = request.form['income']
+        userFeatures[14] = request.form['job']
+        userFeatures[15] = request.form['offspring']
+        userFeatures[16] = request.form['pets']
+        userFeatures[17] = request.form['religion']
+        userFeatures[18] = request.form['smokes']
+        userFeatures[19] = request.form['speaks']
 
-    #oh god
-    userFeatures[0] = request.form['name']
-    userFeatures[1] = request.form['friends']
-    userFeatures[2] = request.form['age']
-    userFeatures[3] = request.form['status']
-    userFeatures[4] = request.form['sex']
-    userFeatures[5] = request.form['orientation']
-    userFeatures[6] = request.form['bodyType']
-    userFeatures[7] = request.form['diet']
-    userFeatures[8] = request.form['drinks']
-    userFeatures[9] = request.form['drugs']
-    userFeatures[10] = request.form['education']
-    userFeatures[11] = request.form['ethnicity']
-    userFeatures[12] = request.form['height']
-    userFeatures[13] = request.form['income']
-    userFeatures[14] = request.form['job']
-    userFeatures[15] = request.form['offspring']
-    userFeatures[16] = request.form['pets']
-    userFeatures[17] = request.form['religion']
-    userFeatures[18] = request.form['smokes']
-    userFeatures[19] = request.form['speaks']
-
-    return render_template("homeLoggedIn.html", handle=userFeatures[0], pog = userFeatures)
+        return render_template("homeLoggedIn.html", handle=userFeatures[0], pog = userFeatures)
+    except IOError:
+        return
+    else:
+        return "Please go back and fill out all forms :("
 
 @app.route("/beginSearch")
 def beginSearch():
@@ -146,7 +150,7 @@ def continueSearch():
 
     training_labels.append(request.form['profileRating'])
 
-    if profilesShown >= 20:
+    if profilesShown >= 2:
         #call function which returns matrix of candidates and their attributes
         return render_template("searchResults.html")
     else:
@@ -155,24 +159,24 @@ def continueSearch():
 
         #the features are labeled incorrectly
         name = features[0]
-        age = features[1]
-        status= features[2]
-        sex= features[3]
-        orientation= features[4]
-        bodyType= features[5]
-        diet = features[6]
-        drinks= features[7]
-        drugs= features[8]
-        education= features[9]
-        ethnicity= features[10]
-        height= features[11]
-        income= features[12]
-        job= features[13]
-        offspring= features[14]
-        pets= features[15]
-        religion= features[16]
-        smokes= features[17]
-        speaks= features[18]
+        age = features[2]
+        status= features[3]
+        sex= features[4]
+        orientation= features[5]
+        bodyType= features[6]
+        diet = features[7]
+        drinks= features[8]
+        drugs= features[9]
+        education= features[10]
+        ethnicity= features[11]
+        height= features[12]
+        income= features[13]
+        job= features[14]
+        offspring= features[15]
+        pets= features[16]
+        religion= features[17]
+        smokes= features[18]
+        speaks= features[19]
         return render_template("displayProfile.html", \
             name = name, \
             age = age, \
