d9a1b3366dc75278c9673cb4432de5033d3e8224
Elijah-Huang <elijahjhuang@gmail.com>
2021-02-16 23:08:46 -0800

    removed signed out link if user is making account; fixed blank submission errors by forcing users to reenter data; after user is created, redirect to mathmake page instead of auto do matchmake; removed beginsearch copy paste via error catching

--- Final_Integrated_Site/app.py
+++ Final_Integrated_Site/app.py
@@ -70,7 +70,7 @@ def uploadData():
 
         session['user features']['name'] = request.form['name']
         session['user features']['neighbors'] = request.form['friends']
-        session['user features']['age'] = request.form['age']
+        session['user features']['age'] = float(request.form['age'])
         session['user features']['status'] = request.form['status']
         session['user features']['sex'] = request.form['sex']
         session['user features']['orientation'] = request.form['orientation']
@@ -80,8 +80,8 @@ def uploadData():
         session['user features']['drugs'] = request.form['drugs']
         session['user features']['education'] = request.form['education']
         session['user features']['ethnicity'] = request.form['ethnicity']
-        session['user features']['height'] = request.form['height']
-        session['user features']['income'] = request.form['income']
+        session['user features']['height'] = float(request.form['height'])
+        session['user features']['income'] = float(request.form['income'])
         session['user features']['job'] = request.form['job']
         session['user features']['offspring'] = request.form['offspring']
         session['user features']['pets'] = request.form['pets']
@@ -89,68 +89,31 @@ def uploadData():
         session['user features']['smokes'] = request.form['smokes']
         session['user features']['speaks'] = request.form['speaks']
 
-        return beginSearch()
+        # create training labels key to be used later for creating user object and feeding to backend
+        # the empty list is because there is no input so we will error and then pop this empty list
+        # and begin normal training
+        session['training labels'] = [[]]
+
+        return continueSearch()
 
     except Exception as e:
-        return "bro u dummy thicc: go back and fill out *ALL* the forms >:C"
-
-@app.route("/beginSearch")
-def beginSearch():
-    # create training labels key to be used later for creating user object and feeding to backend
-    session['training labels'] = []
-
-    features = returnRandomProfile()
-
-    session['training labels'].append([features[-1], 0])
-    session.modified = True
-    
-    name = features[0]
-    age = features[2]
-    status= features[3]
-    sex= features[4]
-    orientation= features[5]
-    bodyType= features[6]
-    diet = features[7]
-    drinks= features[8]
-    drugs= features[9]
-    education= features[10]
-    ethnicity= features[11]
-    height= features[12]
-    income= features[13]
-    job= features[14]
-    offspring= features[15]
-    pets= features[16]
-    religion= features[17]
-    smokes= features[18]
-    speaks= features[19]
-    return render_template("displayProfile.html", \
-        name = name, \
-        age = age, \
-        status= status, \
-        sex= sex, \
-        orientation= orientation, \
-        bodyType= bodyType, \
-        diet = diet, \
-        drinks= drinks, \
-        drugs= drugs, \
-        education= education, \
-        ethnicity= ethnicity, \
-        height= height, \
-        income= income, \
-        job= job, \
-        offspring= offspring, \
-        pets= pets,\
-        religion= religion, \
-        smokes= smokes, \
-        speaks= speaks )
+        return "Please go back and fill out all parts of the form"
 
 
 @app.route("/continueSearch", methods=['GET','POST'])
 def continueSearch():
-    session['training labels'][-1][1] = request.form['profileRating']
-    session.modified = True
+    needs_to_refill = False
+    print("sup")
+    try:
+        print("trying")
+        session['training labels'][-1][1] = float(request.form['profileRating'])
+        print("worked")
+        session.modified = True
+        print(session['training labels'])
+    except Exception as e:
+        needs_to_refill = True
 
-    if len(session['training labels']) >= 20:
+    if not needs_to_refill and len(session['training labels']) >= 20:
         #call function which returns matrix of candidates and their attributes
         nm.add_new_user(session['user features'], session['training labels'])
         # only after we finally create the user and guarantee the csv has been updated do we do this
@@ -159,83 +122,19 @@ def continueSearch():
         # person A then person B actually has person a's id in the population database
         session['id'] = nm.population.shape[0]-1
 
-        compatibleUsers = nm.find_connected_users(session['id'])
+        # after user makes their account they are redirected to the page where they can call match make
+        # before we auto did this, but that adds 80 lines of needless copy paste
+        return render_template('matchmake.html', id=nm.population.loc[session['id'], "name"])
 
-        score1 = compatibleUsers[1][0][0]
-        if compatibleUsers[1][0][1] != -1:
-            name1 = nm.population.loc[compatibleUsers[1][0][1], "name"]
-        else:
-            name1 = "No User Found"
-
-        score2 = compatibleUsers[1][1][0]
-        if compatibleUsers[1][1][1] != -1:
-            name2 = nm.population.loc[compatibleUsers[1][1][1], "name"]
-        else:
-            name2 = "No User Found"
-
-        score3 = compatibleUsers[1][2][0]
-        if compatibleUsers[1][2][1] != -1:
-            name3 = nm.population.loc[compatibleUsers[1][2][1], "name"]
-        else:
-            name3 = "No User Found"
-
-        score4 = compatibleUsers[1][3][0]
-        if compatibleUsers[1][3][1] != -1:
-            name4 = nm.population.loc[compatibleUsers[1][3][1], "name"]
-        else:
-            name4 = "No User Found"
-
-        score5 = compatibleUsers[1][4][0]
-        if compatibleUsers[1][4][1] != -1:
-            name5 = nm.population.loc[compatibleUsers[1][4][1], "name"]
-        else:
-            name5 = "No User Found"
-
-        score6 = compatibleUsers[1][5][0]
-        if compatibleUsers[1][5][1] != -1:
-            name6 = nm.population.loc[compatibleUsers[1][5][1], "name"]
-        else:
-            name6 = "No User Found"
-
-        score7 = compatibleUsers[1][6][0]
-        if compatibleUsers[1][6][1] != -1:
-            name7 = nm.population.loc[compatibleUsers[1][6][1], "name"]
-        else:
-            name7 = "No User Found"
-
-        score8 = compatibleUsers[1][7][0]
-        if compatibleUsers[1][7][1] != -1:
-            name8 = nm.population.loc[compatibleUsers[1][7][1], "name"]
-        else:
-            name8 = "No User Found"
-
-        score9 = compatibleUsers[1][8][0]
-        if compatibleUsers[1][8][1] != -1:
-            name9 = nm.population.loc[compatibleUsers[1][8][1], "name"]
-        else:
-            name9 = "No User Found"
-
-        score10 = compatibleUsers[1][9][0]
-        if compatibleUsers[1][9][1] != -1:
-            name10 = nm.population.loc[compatibleUsers[1][9][1], "name"]
-        else:
-            name10 = "No User Found"
-
-        return render_template("searchResults.html", totalCompatible=compatibleUsers[0], \
-                               name1=name1, score1=score1, \
-                               name2=name2, score2=score2, \
-                               name3=name3, score3=score3, \
-                               name4=name4, score4=score4, \
-                               name5=name5, score5=score5, \
-                               name6=name6, score6=score6, \
-                               name7=name7, score7=score7, \
-                               name8=name8, score8=score8, \
-                               name9=name9, score9=score9, \
-                               name10=name10, score10=score10, 
-                               id=nm.population.loc[session['id'], "name"])
     else:
         features = returnRandomProfile()
 
+        # remove old id for incomplete training label
+        if needs_to_refill:
+            session['training labels'].pop()
+            session.modified = True
+
+        # create new training label
         session['training labels'].append([features[-1], 0])
         session.modified = True
 
@@ -358,7 +257,7 @@ def doMatchmake():
         name7=name7, score7=score7, \
         name8=name8, score8=score8, \
         name9=name9, score9=score9, \
-        name10=name10, score10=score10, 
+        name10=name10, score10=score10,
         id=nm.population.loc[session['id'], "name"])
 
 if __name__ == '__main__':
